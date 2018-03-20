f569aa5721fbbac6f95031a2a44c2dd1cc25955a
Karen G <kgraves0701@gmail.com>
2018-03-20 15:18:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,9 +17,9 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\A[A-Z].+[.,?!]\z/)
    true
- else
+   else
    false
- end
+   end
 end
 
 def valid_phone_number?(phone)
