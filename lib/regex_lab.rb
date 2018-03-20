0874296fe2033805356cfea9e345d0de27b1365b
Karen G <kgraves0701@gmail.com>
2018-03-20 14:52:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  word.scan(/\b[aeiou]/).nil? ? 'false' : 'true'
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun/)
 end
 
 def words_five_letters_long(text)
-
+  #text.scan()
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
